573bb07677c0a14bf1f5931213dd37ecd9d814f1
Danilo Kulisic <danilokulisic4@gmail.com>
2023-03-17 13:10:42 +0100

    adding styles and description

--- Desktop/next-portfolio/pages/index.js
+++ Desktop/next-portfolio/pages/index.js
@@ -17,36 +17,36 @@ export default function Home() {
         <p className={styles.description}>
           I am web developer
         </p>
-        <p>
+        <p className={styles.description}>
           I will bring your ideas, and work to life on the web.
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>About &rarr;</h3>
+            <p>Find out more about my experience as a Developer.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>My skills &rarr;</h3>
+            <p>Read about my skills in various computer languages.</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Work &rarr;</h3>
+            <p>Here you can get ideas for your bussines. You can test out the projects that I have already made.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Contact &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Feel free to get in touch with me! You can ask me anything you want!
             </p>
           </a>
         </div>
